d8beebf5199c1db8c80b938e048567bb104ff509
smoonmare <smoonmare@gmail.com>
2023-07-28 16:09:34 -0700

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'angular-animation-demo';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item: string) {
+      this.list.push(item);
+    }
 }
